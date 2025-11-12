c7879176a2b7dd712f24b634d0e54085faac8c19
4rcan31 <da.esfra12@gmail.com>
2025-11-12 17:22:56 -0600

    Fix bug in HttpResponse, normalizate data key

--- http/HttpResponse.ts
+++ http/HttpResponse.ts
@@ -2,13 +2,23 @@ import { Response } from "express";
 
 
 export class HttpResponse {
-  static success(res: Response, data: any, message: string = 'Operación exitosa', statusCode: number = 200) {
+  static success(res: Response, data: any, message = "Operación exitosa", statusCode = 200) {
+    if (data && data.data && data.pagination) {
+      return res.status(statusCode).json({
+        success: true,
+        message,
+        ...data,
+        statusCode
+      });
+    }
+
     return res.status(statusCode).json({
       success: true,
       message,
       data,
-      statusCode : statusCode
+      statusCode
     });
   }
+
 }
 
