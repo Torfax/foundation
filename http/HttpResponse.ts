1201943951fcffcf47f267ad0d8c055d8d6e7534
4rcan31 <da.esfra12@gmail.com>
2025-11-24 11:04:58 -0600

    refactor(core/http): update HttpResponse to wrap success and error data using payload key

--- http/HttpResponse.ts
+++ http/HttpResponse.ts
@@ -3,19 +3,19 @@ import { Response } from "express";
 
 export class HttpResponse {
   static success(res: Response, data: any, message = "Operación exitosa", statusCode = 200) {
-    if (data && data.data && data.pagination) {
-      return res.status(statusCode).json({
-        success: true,
-        message,
-        ...data,
-        statusCode
-      });
-    }
-
     return res.status(statusCode).json({
       success: true,
       message,
-      data,
+      payload: data,
+      statusCode
+    });
+  }
+
+  static err(res: Response, data: any, message = "Operación fallida", statusCode = 400) {
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      payload: data,
       statusCode
     });
   }
