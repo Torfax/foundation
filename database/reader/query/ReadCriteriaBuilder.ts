a955a8661e4ea015645890a0d7b04b16e2b194ff
4rcan31 <da.esfra12@gmail.com>
2026-02-26 10:32:01 -0600

    Improve ReadCriteriaBuilder typing to support EntityFields generics

--- database/reader/query/ReadCriteriaBuilder.ts
+++ database/reader/query/ReadCriteriaBuilder.ts
@@ -4,37 +4,44 @@ import { PaginationCriteria } from "../criteria/PaginationCriteria";
 import { SortCriteria } from "../criteria/SortCriteria";
 import { ListQueryDto } from "@src/core/database/reader/query/ListQueryDto";
 
+export type EntityFields<T> = keyof T & string;
+
 export class ReadCriteriaBuilder {
 
-    static fromDto(dto: ListQueryDto): ReadCriteria {
+    static fromDto<T extends string>(
+        dto?: ListQueryDto
+    ): ReadCriteria<T> | undefined {
+
+        if (!dto) return undefined;
+
         return {
-            filters: this.buildFilters(dto),
-            sort: this.buildSort(dto),
+            filters: this.buildFilters<T>(dto),
+            sort: this.buildSort<T>(dto),
             pagination: this.buildPagination(dto)
         };
     }
 
-    private static buildFilters(
+    private static buildFilters<T extends string>(
         dto: ListQueryDto
-    ): FilterCriteria[] | undefined {
+    ): FilterCriteria<T>[] | undefined {
 
         if (!dto.filters?.length) return undefined;
 
         return dto.filters.map((f) => ({
-            field: f.field,
+            field: f.field as T,
             operator: f.op,
             value: f.value
         }));
     }
 
-    private static buildSort(
+    private static buildSort<T extends string>(
         dto: ListQueryDto
-    ): SortCriteria[] | undefined {
+    ): SortCriteria<T>[] | undefined {
 
         if (!dto.sort) return undefined;
 
         return [{
-            field: dto.sort.field,
+            field: dto.sort.field as T,
             direction: dto.sort.direction
         }];
     }
