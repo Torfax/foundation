96515377d4d6ebb81033dae904e65d7f2d01485d
4rcan31 <da.esfra12@gmail.com>
2026-03-02 11:44:44 -0600

    Update modules to use utils as ES module instead of DI

--- AppCore.ts
+++ AppCore.ts
@@ -1,5 +1,4 @@
 import { ConfigService } from "@src/core/config/ConfigService";
-import { UtilsModule } from "./utils";
 import { FilesModule, FilesModuleOptions } from "./files";
 
 export interface AppCoreOptions {
@@ -10,18 +9,11 @@ export interface AppCoreOptions {
 export class AppCore {
 
     public readonly configService: ConfigService;
-    public readonly utilsModule: UtilsModule;
     public readonly filesModule: FilesModule;
 
     constructor(options: AppCoreOptions = {}) {
         this.configService = new ConfigService();
-        this.utilsModule = new UtilsModule();
         this.filesModule = new FilesModule(options.files)
-    }
-
-
-    get utils() {
-        return this.utilsModule.api;
     }
 
     get files() {
