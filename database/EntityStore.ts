a70474bbb15e8db64fb31eb70525d7107836a634
4rcan31 <da.esfra12@gmail.com>
2026-03-10 11:38:02 -0600

    Improve EntityStore raw method typing

--- database/EntityStore.ts
+++ database/EntityStore.ts
@@ -1,3 +1,4 @@
+import { Repository } from "typeorm";
 import { DataSourceDriver } from "./adapters/DataSourceDriver";
 import { EntityConstructor } from "./EntityConstructor";
 import { Reader } from "./reader/Reader";
@@ -22,9 +23,21 @@ export class EntityStore<E extends object> {
   }
 
   async raw<R>(
-    operation: (repo: any) => Promise<R>
+
+    /* 
+      Notice/Warn/TODO:
+
+      El tipo Repository realmente no se deberia de
+      ocupar aca.. ya que este objeto pertenece 
+      a typeorm y se supone que el core debe
+      de ser agnostico al orm
+
+      sin embargo actualmente solo se esta trabajando
+      con typeorm, por eso funcionara asi por el momento
+    */
+    operation: (repo: Repository<E>) => Promise<R>
   ): Promise<R> {
-    return this.driver.raw(this.entity, operation);
+    return this.driver.raw<E, R>(this.entity, operation);
   }
 }
 
